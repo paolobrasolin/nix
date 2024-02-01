1662d5d09b81c7076ffe492b43b91499b0ff05b1
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-02-02 00:44:57 +0100

    Try out flakes

---
+++ machines/inari/flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "Paolo's NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      "inari" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}

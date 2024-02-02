b950e45bad51219a4294babaf474051403279424
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-02-02 01:13:03 +0100

    Stub home manager setup

--- flake.nix
+++ flake.nix
@@ -3,14 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
-  outputs = { self, nixpkgs, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       "inari" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/inari/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # home-manager.users.paolo = import ./users/paolo.nix;
+          }
         ];
       };
     };

21d1d9cf69e5a40a6593e41fb8180216b4262933
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-02-19 01:07:40 +0100

    Improve codeium keybindings

--- users/paolo/files/lvim_config.lua
+++ users/paolo/files/lvim_config.lua
@@ -109,6 +109,13 @@ lvim.plugins = {
       vim.g.codeium_bin = "/run/current-system/sw/bin/codeium_language_server"
       vim.g.codeium_enabled = true
       vim.g.codeium_manual = false
+      vim.keymap.set('i', '<M-Enter>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<M-Backspace>', function() return vim.fn['codeium#Clear']() end,
+        { expr = true, silent = true })
     end
   }
 }

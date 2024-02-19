bf935b2a59b66186efe289397691102107806162
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-02-19 22:49:52 +0100

    Improve ARM documentation

--- hosts/inari/aarch64.nix
+++ hosts/inari/aarch64.nix
@@ -1,6 +1,9 @@
 {...}: {
   # This is required to successfully provision an aarch64-linux system.
   # See https://nixos.wiki/wiki/Install_NixOS_on_Hetzner_Cloud#AArch64_.28CAX_instance_type.29_specifics
+
+  # initialize the display early to get a complete log
   boot.initrd.kernelModules = [ "virtio_gpu" ];
+  # workaround because the console defaults to serial
   boot.kernelParams = [ "console=tty" ];
 }

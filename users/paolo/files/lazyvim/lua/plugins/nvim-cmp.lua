b0bf2af2e8956d7714e355ffc25c3d094373fb8f
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-05-23 12:36:41 +0200

    Emojis yay

--- users/paolo/files/lazyvim/lua/plugins/nvim-cmp.lua
+++ users/paolo/files/lazyvim/lua/plugins/nvim-cmp.lua
@@ -2,9 +2,11 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
+			{ "hrsh7th/cmp-emoji" },
 			{ "Arkissa/cmp-agda-symbols" },
 		},
 		opts = function(_, opts)
+			table.insert(opts.sources, { name = "emoji" })
 			table.insert(opts.sources, { name = "agda-symbols" })
 		end,
 	},

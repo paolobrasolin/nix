da79eaeccc1b42b8b9c77a31c957147b678bdc93
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-05-31 21:50:39 +0200

    Disable agda TS

--- users/paolo/files/lazyvim/lua/plugins/nvim-treesitter.lua
+++ users/paolo/files/lazyvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,17 @@ return {
 			-- NOTE: we just install gcc via nix and let nvim-treesitter do its thing.
 			auto_install = true,
 			-- ensure_installed = {},
-			ignore_install = { "agda" },
+			highlight = {
+				enable = true,
+				disable = { "agda" },
+				-- disable = function(lang, buf)
+				-- 	local file_path = vim.api.nvim_buf_get_name(buf)
+				-- 	-- Disable highlighting for Agda files outside of the Nix store
+				-- 	if lang == "agda" and not string.find(file_path, "^/nix/store/") then
+				-- 		return true
+				-- 	end
+				-- end,
+			},
 		},
 	},
 }

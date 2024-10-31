d8f29e3eb4374aff7901048b2f066bec976bf673
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-10-31 16:30:02 +0200

    [ebisu] improve global git config

--- users/paolo/ebisu.nix
+++ users/paolo/ebisu.nix
@@ -1,20 +1,26 @@
 {
-  programs.git.includes = (map (ns: {
-    condition = "gitdir:~/${ns}/";
-    contents = {
-      user.name = "Paolo Brasolin";
-      user.email = "paolo.brasolin@gmail.com";
-      core.sshCommand = "ssh -F /dev/null -i ~/.ssh/pb-paolo_at_ebisu";
-    };
-  }) ["pb" "vp" "bi" "ef" "uc"]) ++ [
-    {
-      condition = "gitdir:~/dq/";
+  programs.git.includes =
+    (map (ns: {
+      condition = "gitdir:~/${ns}/";
       contents = {
         user.name = "Paolo Brasolin";
-        user.email = "paolo.brasolin@donq.io";
-        core.sshCommand = "ssh -F /dev/null -i ~/.ssh/dq-paolo_at_ebisu";
+        user.email = "paolo.brasolin@gmail.com";
+        core.sshCommand = "ssh -F /dev/null -i ~/.ssh/pb-paolo_at_ebisu";
       };
-    }
+    }) ["pb" "vp" "bi" "ef" "uc"])
+    ++ [
+      {
+        condition = "gitdir:~/dq/";
+        contents = {
+          user.name = "Paolo Brasolin";
+          user.email = "paolo.brasolin@donq.io";
+          core.sshCommand = "ssh -F /dev/null -i ~/.ssh/dq-paolo_at_ebisu";
+        };
+      }
+    ];
+  programs.git.ignores = [
+    ".aider.*"
+    ".direnv/"
   ];
   # NOTE:these are to be paired with some `includes.*.contents.url`s as an alternative to `sshCommand` but tbh I find the whole thing quite cursed:
   # # url."paolobrasolin_at_github.com".insteadOf = "git@github.com";

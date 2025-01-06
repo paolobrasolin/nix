c66d84849eb258be15147a05c460f1bbb3354aa3
Paolo Brasolin <paolo.brasolin@gmail.com>
2025-01-06 18:36:46 +0100

    Config swagda bindings

--- users/paolo/files/lazyvim/lua/plugins/swagda.lua
+++ users/paolo/files/lazyvim/lua/plugins/swagda.lua
@@ -2,5 +2,14 @@ return {
 	{
 		"paolobrasolin/swagda.nvim",
 		dir = "~/pb/swagda.nvim",
+		config = function()
+			require("swagda").setup()
+			-- vim.keymap.set(
+			-- 	"v",
+			-- 	"<leader>aH",
+			-- 	":<C-u>'<,'>SwagdaSimplifySelection<cr>",
+			-- 	{ desc = "Simplify selection", buffer = true }
+			-- )
+		end,
 	},
 }
